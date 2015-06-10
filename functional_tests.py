f47e6ab0f35cdcf0a21eec6d81f2b932aed60464
Greg Smith <gregs@6ft.com>
2015-06-10 11:52:28 -0500

    Add submit to form

--- functional_tests.py
+++ functional_tests.py
@@ -10,6 +10,11 @@ class NewVisitorTest(unittest.TestCase):
   def tearDown(self):
     self.browser.quit()
 
+  def check_for_row_in_list_table(self, row_text):
+    table = self.browser.find_element_by_id('id_list_table')
+    rows = table.find_element_by_tag_name('tr')
+    self.assertIn(row_text, [row.text for row in rows])
+
   def test_can_start_a_list_and_retrieve_it_later(self):
     #Open a browser to our homepage
     self.browser.get('http://localhost:3333')
@@ -31,17 +36,7 @@ class NewVisitorTest(unittest.TestCase):
     # On enter, the page updates and lists 
     # "1: Buy Peacock Feathers" as a new to-do list item
     inputbox.send_keys(Keys.ENTER)
-
-    import time
-    time.sleep(10)
-
-    table = self.browser.find_element_by_id('id_list_table')
-    rows = table.find_elements_by_tag_name('tr')
-    self.assertIn('1: Buy Peacock Feathers', [row.text for row in rows])
-    self.assertIn(
-        '2: Use peacock feathers to make a fly' ,
-         [row.text for row in rows]
-    )
+    self.check_for_row_in_list_table('1: Buy Peacock Feathers')
 
     # There is still a text box inviting her to add another item. She
     # enters "Use peacock feathers to make a fly" (Edith is very
@@ -49,15 +44,9 @@ class NewVisitorTest(unittest.TestCase):
     inputbox = self.browser.find_element_by_id('id_new_item')
     inputbox.send_keys('Use peacock feathers to make a fly')
     inputbox.send_keys(Keys.ENTER)
+    self.check_for_row_in_list_table('1: Buy Peacock Feathers')
+    self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
-    # The page updates again, and now shows both items on her list
-    table = self.browser.find_element_by_id('id_list_table')
-    rows = table.find_elements_by_tag_name('tr')
-    self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-    self.assertIn(
-        '2: Use peacock feathers to make a fly' ,
-         [row.text for row in rows]
-    )
     self.fail('Finish the test!')
     # The page updates and shows both items
 
