8679344cfa29a32449ffaf7f0b8aca2bc6d0a2c8
Greg Smith <gregory.terrell.smith@gmail.com>
2015-06-15 15:45:35 -0500

    Hack FT runner to be able to test staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,23 @@
+import sys
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 class NewVisitorTest(StaticLiveServerTestCase):
+  @classmethod
+  def setUpClass(cls):
+    for arg in sys.argv:
+      if 'liveserver' in arg:
+        cls.server_url = 'http://' + arg.split('=')[1]
+        return
+    super().setUpClass()
+    cls.server_url = cls.live_server_url
+
+  @classmethod
+  def tearDownClass(cls):
+    if cls.server_url == cls.live_server_url:
+      super().tearDownClass()
+
   def setUp(self):
       self.browser = webdriver.Firefox()
       self.browser.implicitly_wait(3)
@@ -12,7 +27,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
   def test_layout_and_styling(self):
     # Head to the homepage
-    self.browser.get(self.live_server_url)
+    self.browser.get(self.server_url)
     self.browser.set_window_size(1024,768)
 
     # Notice a nice centered input bx
@@ -39,7 +54,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
   def test_can_start_a_list_and_retrieve_it_later(self):
     #Open a browser to our homepage
-    self.browser.get(self.live_server_url)
+    self.browser.get(self.server_url)
 
     #Check out how the title mentions to-do lists
     self.assertIn('To-Do', self.browser.title)
@@ -81,7 +96,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     # Francis visits the home page.  There is no sign of Edith's
     # list
-    self.browser.get(self.live_server_url)
+    self.browser.get(self.server_url)
     page_text = self.browser.find_element_by_tag_name('body').text
     self.assertNotIn('Buy Peacock Feathers', page_text)
     self.assertNotIn('make a fly', page_text)
