4a738527243140ec85a233bad366b28adbee2c08
Greg Smith <gregory.terrell.smith@gmail.com>
2015-07-13 14:02:33 -0500

    Enforce model validation in list view

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -1,32 +1,41 @@
 from django.test import TestCase
+from django.core.exceptions import ValidationError 
 from lists.models import Item, List
 
 class ListAndItemModelTest(TestCase):
-  def test_saving_and_retrieving_items(self):
-    list_ = List()
-    list_.save()
+    def test_cannot_save_empty_list_items(self):
+        list_ = List.objects.create()
+        item = Item(list=list_, text='')
+        with self.assertRaises(ValidationError):
+            item.save()
+            item.full_clean()
 
-    first_item = Item()
-    first_item.text = 'The first (ever) list item'
-    first_item.list = list_
-    first_item.save()
 
-    second_item = Item()
-    second_item.text = 'The 2nd (ever) list item'
-    second_item.list = list_
-    second_item.save()
+    def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
 
-    saved_list = List.objects.first()
-    self.assertEqual(saved_list, list_)
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.list = list_
+        first_item.save()
 
-    saved_items = Item.objects.all()
-    self.assertEqual(saved_items.count(), 2)
+        second_item = Item()
+        second_item.text = 'The 2nd (ever) list item'
+        second_item.list = list_
+        second_item.save()
 
-    first_saved_item = saved_items[0]
-    second_saved_item = saved_items[1]
-    self.assertEqual(first_saved_item.text, 'The first (ever) list item')
-    self.assertEqual(first_saved_item.list, list_)
-    self.assertEqual(second_saved_item.text, 'The 2nd (ever) list item')
-    self.assertEqual(second_saved_item.list, list_)
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(first_saved_item.list, list_)
+        self.assertEqual(second_saved_item.text, 'The 2nd (ever) list item')
+        self.assertEqual(second_saved_item.list, list_)
 
 
