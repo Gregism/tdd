3426eae467fd2841c1c02a3f18aff70d0a74ab9c
Greg Smith <gregory.terrell.smith@gmail.com>
2016-09-26 10:45:45 -0500

    Create a new lists url for creating new lists

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -5,6 +5,25 @@ from django.template.loader import render_to_string
 from lists.views import home_page
 from lists.models import Item
 
+class NewListTest(TestCase):
+  def test_saving_a_test_response(self):
+    self.client.post(
+      '/lists/new',
+      data={'item_text': 'A new list item'}
+    )
+    self.assertEqual(Item.objects.count(), 1)
+    new_item = Item.objects.first()
+    self.assertEqual(new_item.text, 'A new list item')
+
+  def test_homepage_redirects_after_POST(self):
+    response = self.client.post(
+      '/lists/new',
+      data={'item_text': 'A new list item'}
+    )
+
+    self.assertRedirects(response,'/lists/the-only-list-in-the-world/')
+
+
 class ListViewTest(TestCase):
   def test_uses_list_template(self):
     response = self.client.get('/lists/the-only-list-in-the-world/')
@@ -48,26 +67,6 @@ class HomePageTest(TestCase):
     home_page(request)
     self.assertEqual(Item.objects.count(),0)
 
-  def test_homepage_can_save_a_POST_request(self):
-    request = HttpRequest()
-    request.method = 'POST'
-    request.POST['item_text'] = 'A new list item'
-
-    response = home_page(request)
-
-    self.assertEqual(Item.objects.count(), 1)
-    new_item = Item.objects.first()
-    self.assertEqual(new_item.text, 'A new list item')
-
-  def test_homepage_redirects_after_POST(self):
-    request = HttpRequest()
-    request.method = 'POST'
-    request.POST['item_text'] = 'A new list item'
-
-    response = home_page(request)
-
-    self.assertEqual(response.status_code, 302)
-    self.assertEqual(response['location'],'/lists/the-only-list-in-the-world/')
     
     # self.assertIn('A new list item', response.content.decode())
     # expected_html = render_to_string(
